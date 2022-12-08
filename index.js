109c58aaca57060937b2c0e4871104fe4cb9ad0f
Gabriel Lopes Ribeiro da Costa <116298645+GabrielLopesz9135@users.noreply.github.com>
2022-12-08 14:39:17 +0000

    Segunda Aula Pratica JavaScript

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var salario_bruto = 7500;
+const prompt = require('prompt-sync')();
+
+nome = String(prompt("Digite seu Nome: "));
+salario_bruto = parseInt(prompt("Digite seu salario Bruto:"));
 var ir = salario_bruto * 0.10;
 var salario_liquido = salario_bruto - ir;
 console.log("O salario liquido é igual a: ", salario_liquido);
