b5e2905e3a31b08a3872e7bb95f01dd789c6832a
Gabriel Lopes Ribeiro da Costa <116298645+GabrielLopesz9135@users.noreply.github.com>
2022-12-22 16:31:04 +0000

    Aula 09 JavaScript

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const prompt = require('prompt-sync')();
 
-nome = String(prompt("Digite seu Nome: "));
-salario_bruto = parseInt(prompt("Digite seu salario Bruto:"));
-var ir = salario_bruto * 0.10;
-var salario_liquido = 0;
-if( salario_bruto < 1903.98){
-   salario_liquido = salario_bruto * 0.95;
+for(var i = 1; i <= 5; i++){
+  var nome = prompt("Digite seu nome: ");
+  var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
+  var dependentes = parseInt(prompt("Digite o número de dependentes: "));
+  
+  var renda_percapta = salario_bruto / (dependentes + 1);
+  
+  if (renda_percapta >= 500) {
+  
+    if (salario_bruto > 0 && salario_bruto <= 1903.98) {
+      var ir = salario_bruto * 0.05;
+    } else if (salario_bruto <= 2826.65) {
+      var ir = salario_bruto * 0.075;
+    } else {
+      var ir = salario_bruto * 0.15;
+    }
+  
+  } else {
+    var ir = 0;
+  }
+  
+  var salario_liquido = salario_bruto - ir;
+  console.log("Salário líquido: R$", salario_liquido);
 }
-else if(salario_bruto < 2826.65){
-   salario_liquido = salario_bruto * 0.925;
-}
-else if( salario_bruto > 2826.66){
-   salario_liquido = salario_bruto * 0.85;
-}
-console.log("O salario liquido é igual a: ", salario_liquido);
-
-
-// salário bruto (de 0,00 até 1.903,98): 5%
-// salário bruto (de 1.903,99 até 2.826,65): 7,5%
-// salário bruto (a partir de 2.826,66): 15%
